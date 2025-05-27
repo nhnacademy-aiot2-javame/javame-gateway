a3c60af025e6bb5814f578294edf15425c17dfa3
ATGN02-018 <ygw1026@naver.com>
2025-05-27 17:11:43 +0900

    local docker 환경에서 테스트

--- src/main/java/com/nhnacademy/javamegateway/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/nhnacademy/javamegateway/filter/JwtAuthenticationFilter.java
@@ -33,7 +33,7 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
     private static final List<String> WHITE_LIST = List.of(
             "/api/v1/auth/register",
             "/api/v1/auth/login",
-            "/api/v1/environment"
+            "/api/v1/environment",
             "/api/v1/members/register",
             "/api/v1/members/register/owners",
             "/api/v1/companies/register",
